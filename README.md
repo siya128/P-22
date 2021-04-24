cb924891059690e8704ff7da3d621893f83f9328
siya128 <78148317+siya128@users.noreply.github.com>
2021-04-24 19:52:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# P-22
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,40 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+  var ground_options={
+      isStatic: true
+  }  
+
+    ground = Bodies.rectangle(200,380,200,20,ground_options);
+    World.add(world,ground);
+
+     var ball_options = {
+         restitution : 0.7
+
+     }
+ball = Bodies.circle(200,100,20,ball_options)
+World.add(world,ball)
+   
+
+    console.log(ground);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    fill("yellow")
+    rect(ground.position.x,ground.position.y,400,20);
+    fill("green")
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
+}
